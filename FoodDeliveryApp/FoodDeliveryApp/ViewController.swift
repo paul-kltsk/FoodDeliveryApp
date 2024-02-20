02fa3da1615a726b24453102f102a64336085ebd
pshkltsk <pshkltsk@gmail.com>
2024-02-20 13:14:14 +0300

    Added new fonts

--- FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -11,8 +11,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .green
+        
+        view.backgroundColor = .systemGray
+        
+        let label = UILabel()
+        label.text = "Hello world!!!"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            label.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
+        label.font = .Roboto.thin.size(of: 40)
     }
 
 
