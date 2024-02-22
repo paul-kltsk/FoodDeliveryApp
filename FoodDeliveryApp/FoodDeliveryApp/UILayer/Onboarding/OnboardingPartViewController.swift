db25a6c4c46fccecf5f0b1fd035a63c97155d785
pshkltsk <pshkltsk@gmail.com>
2024-02-22 18:52:00 +0300

    Added onboarding view controller

---
+++ FoodDeliveryApp/FoodDeliveryApp/UILayer/Onboarding/OnboardingPartViewController.swift
@@ -0,0 +1,107 @@
+//
+//  OnboardingPathViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Pavel Kylitsky on 22/02/2024.
+//
+
+import UIKit
+
+class OnboardingPartViewController: UIViewController {
+    
+    //MARK: - Properties
+    var image: UIImage? {
+        didSet {
+            imageView.image = image
+        }
+    }
+    
+    var titleText: String? {
+        didSet{
+            titleLabel.text = titleText
+        }
+    }
+    
+    var descriptionText: String? {
+        didSet {
+            descriptionLabel.text = descriptionText
+        }
+    }
+    
+    
+    
+    //MARK: - Views
+    private let imageView = UIImageView()
+    private let titleLabel = UILabel()
+    private let descriptionLabel = UILabel() 
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupLayout()
+        
+    }
+    
+    
+}
+
+
+//MARK: - Layout
+private extension OnboardingPartViewController {
+    
+    func setupLayout() {
+        setupView()
+        setupImageView()
+        setupTitleLabel()
+        setupDescriptionLabel()
+    }
+    
+    func setupView() {
+        view.backgroundColor = AppColors.accentOrange
+    }
+    
+    func setupImageView() {
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 200),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+        ])
+    }
+    
+    func setupTitleLabel() {
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = .Roboto.bold.size(of: 24)
+        titleLabel.textColor = .white
+        titleLabel.textAlignment = .center
+        titleLabel.numberOfLines = 2
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
+    }
+    
+    func setupDescriptionLabel() {
+        view.addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.font = .Roboto.regular.size(of: 14)
+        descriptionLabel.textColor = .white
+        descriptionLabel.textAlignment = .center
+        descriptionLabel.numberOfLines = 2
+        
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 23),
+            descriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
+    }
+    
+}
